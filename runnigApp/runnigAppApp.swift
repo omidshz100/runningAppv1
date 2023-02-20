e2af44d2aedd4cd6d8e88bb2fdd0a24585952555
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-02-20 16:31:30 +0100

    Initial Commit

---
+++ runnigApp/runnigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  runnigAppApp.swift
+//  runnigApp
+//
+//  Created by Omid Shojaeian Zanjani on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct runnigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
